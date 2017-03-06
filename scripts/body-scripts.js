0f940011027396ec929bd518d11b3b56b7914fb5
Steven A Chan <steven.a.chan@gov.ab.ca>
2017-03-06 16:50:09 -0700

    General website functionality working

---
+++ scripts/body-scripts.js
@@ -0,0 +1,9 @@
+$(function(){
+    $(window).on("load resize", function(){
+        $(".fill-screen").css("height",window.innerHeight);
+    });
+
+    $('body').scrollspy({
+        target: '.navbar'
+    })
+});
