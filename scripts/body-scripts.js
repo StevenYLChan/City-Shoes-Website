2bc75ddcfe7fd0be5d92a448bf35d1697b18caa0
Steven A Chan <steven.a.chan@gov.ab.ca>
2017-03-07 12:02:18 -0700

    General layout complete

--- scripts/body-scripts.js
+++ scripts/body-scripts.js
@@ -4,6 +4,35 @@ $(function(){
     });
 
     $('body').scrollspy({
-        target: '.navbar'
-    })
+        target: '.navbar',
+        offset: 160
+    });
+
+    $('nav a, .down-button a').bind('click',function(){
+    	$('html, body').stop().animate({
+    		scrollTop: $($(this).attr('href')).offset().top -100
+    	}, 1500, 'easeInOutExpo');
+    	event.preventDefault();
+    	});
+
+    $(window).stellar();
+
+    new WOW().init();
+
+    $(document).ready(function () {
+        $("#nanoGallery3").nanoGallery({
+
+                thumbnailHeight: 250,
+                colorScheme: 'light',
+                colorSchemeViewer: 'light',
+                thumbnailHoverEffect: {name: 'scale120', duration: 100},
+                theme: 'clean',
+                thumbnailGutterWidth : 0,
+                thumbnailGutterHeight : 0,
+                // i18n: { thumbnailImageDescription: 'View Photo', thumbnailAlbumDescription: 'Open Album' },
+                 thumbnailLabel: { display: false, position: 'overImageOnMiddle', align: 'center'},
+                thumbnailOpenImage: false,
+
+        });
+    });
 });
